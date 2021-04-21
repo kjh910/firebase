6d70be68d1446928b6700f0e3a8f61c7537b576b
kjh910 <god12nom@gmail.com>
2021-04-21 16:44:52 +0900

    6

--- test/src/App.js
+++ test/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          횬주 따랑해!!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn
         </a>
       </header>
     </div>
